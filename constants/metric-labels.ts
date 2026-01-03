91e6ad2767827179643592f1d4cb3cc2adc7731c
Veer Shah <shahveerkeaten@gmail.com>
2026-01-03 15:36:20 +0530

    Add metric labels constants

---
+++ constants/metric-labels.ts
@@ -0,0 +1,8 @@
+export const METRIC_LABELS = {
+  usageOfKeywords: "Usage of Keywords",
+  pronunciation: "Pronunciation",
+  fluency: "Fluency",
+  objectionHandling: "Objection Handling",
+  queryResolution: "Query Resolution",
+  eyeContact: "Eye Contact"
+} as const
